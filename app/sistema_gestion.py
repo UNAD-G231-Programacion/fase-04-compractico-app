b509e039aba5b35c62bebd679edc77ba8cbbae0d
daniel-benavides <210926315+dannxdev@users.noreply.github.com>
2026-05-04 08:23:47 -0500

    nuevoArchivo; Se creo el archivo 'sistema_gestion.py'...
    Este archivo es la estructura para organizar los aportes entregados
    por cada colaborador, para la aplicacion final.

---
+++ app/sistema_gestion.py
@@ -0,0 +1,41 @@
+# ============================================================
+# SISTEMA INTEGRAL DE GESTIÓN DE CLIENTES, SERVICIOS Y RESERVAS
+# Universidad Nacional Abierta y a Distancia - UNAD
+# Curso: Programación 213023 - Grupo: 231
+# Integrantes:
+#   - Daniel Benavides  → Módulo: Servicios Especializados
+#   - Johan Pérez       → Módulo: Reservas
+#   - Andrés Maya       → Módulo: Arquitectura Base y Gestion de Clientes
+#   - Sebastian Gonzales     → Módulo: Calculo de Costos
+#   - Jaider Pantoja     → Módulo: No Seleccionado, sin contribuciones
+# ============================================================
+
+
+# ── IMPORTACIONES ───────────────────────────────────────────
+# Importaciones que se hacen al codigo
+
+
+# ── MÓDULO 1: SERVICIOS ESPECIALIZADOS (Autor: Daniel Benavides) ───
+# (código de Daniel)
+
+
+# ── MÓDULO 2: RESERVAS (Autor: Johan Pérez) ────────
+# (Codigo de Johan)
+
+
+# ── MÓDULO 3: ARQUITECTURA BASE Y GESTION DE CLIENTES (Autor: Andrés Maya) ────────
+# (código de Andrés)
+
+
+# ── MÓDULO 4: CALCULO DE COSTOS (Autor: Sebastian Gonzales) ────────
+# (código del Sebastian Gonzales)
+
+
+# ── MÓDULO 5: SISTEMA DE LOGS Y SIMULACION (Autor: Jaider Pantoja) ────────
+# (código del Jaider Pantoja)
+
+
+# ── PUNTO DE ENTRADA ─────────────────────────────────────────
+if __name__ == "__main__":
+    # Para iniciar la app.
+    pass
